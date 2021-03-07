e931f4aa30c8af5671befef27fb1a57bc3870315
Kasra_K2K <Kasra_K2K@yahoo.com>
2021-03-07 01:36:21 -0800

    remove audio

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const ffmpeg = require("fluent-ffmpeg");
 ffmpeg.setFfmpegPath(ffmpegPath);
 
 const videoToImage = (videoPath, folder, filename, times, count = 1) => {
-  return new ffmpeg(videoPath).takeScreenshots(
+  return new ffmpeg(videoPath).noAudio().takeScreenshots(
     {
       count,
       timemarks: typeof times === "number" ? [times] : times,
