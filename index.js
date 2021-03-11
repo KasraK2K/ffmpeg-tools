888dce2c14d1a1c2f73d866f111d218bf7c69ece
Kasra_K2K <Kasra_K2K@yahoo.com>
2021-03-12 00:05:53 +0330

    add watermark tool

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 "use strict";
 exports.__esModule = true;
-exports.snapshot = void 0;
+exports.watermark = exports.snapshot = void 0;
 var ffmpeg_1 = require("@ffmpeg-installer/ffmpeg");
 var ffmpeg = require("fluent-ffmpeg");
 ffmpeg.setFfmpegPath(ffmpeg_1.path);
+var path_1 = require("path");
 /**
  * @param {*} videoPath: Input video file string path
  * @param {*} folder: Destination folder to save snapshot
@@ -21,3 +22,31 @@ var snapshot = function (videoPath, folder, filename, times, count) {
     }, folder);
 };
 exports.snapshot = snapshot;
+/**
+ * @param {*} video: Selected Video to place watermark on it
+ * @param {*} watermark: Object like { dir: string; icon: string } to find watermark picture
+ * @param {*} destination: Destination folder to save watermarked video
+ * @param {*} overlay: Position of watermark like (W-w)-20:(H-h)-40
+ * @return {*}
+ */
+var watermark = function (video, watermark, destination, overlay) {
+    if (overlay === void 0) { overlay = "(W-w)-20:(H-h)-40"; }
+    var iconPath = path_1.join(watermark.dir, watermark.icon);
+    return new ffmpeg({ source: video })
+        .addOption("-vf", "movie=" + iconPath + " [watermark]; [in] [watermark] overlay=" + overlay + " [out]")
+        .on("start", function (commandLine) {
+        console.log(commandLine);
+    })
+        .on("progress", function (progress) {
+        console.log(progress.timemark);
+    })
+        .on("error", function (err, stdout, stderr) {
+        console.log("ERROR: " + err.message);
+        console.log("STDERR:" + stderr);
+    })
+        .on("end", function () {
+        console.log("DONE");
+    })
+        .saveToFile(destination);
+};
+exports.watermark = watermark;
--- index.ts
+++ index.ts
@@ -1,13 +1,14 @@
 import { path as ffmpegPath } from "@ffmpeg-installer/ffmpeg";
 import * as ffmpeg from "fluent-ffmpeg";
 ffmpeg.setFfmpegPath(ffmpegPath);
+import { join } from "path";
 
 /**
- * @param {*} videoPath: Input video file string path
- * @param {*} folder: Destination folder to save snapshot
- * @param {*} filename: Snapshot image name
- * @param {*} times: Seconds to take snapshot
- * @param {number} [count=1]: The number of snapshot should to create
+ * @param {*} videoPath : Input video file string path
+ * @param {*} folder : Destination folder to save snapshot
+ * @param {*} filename : Snapshot image name
+ * @param {*} times : Seconds to take snapshot
+ * @param {number} [count=1] : The number of snapshot should to create
  * @return {*}
  */
 export const snapshot = (
@@ -25,4 +26,39 @@ export const snapshot = (
     },
     folder
   );
+};
+
+/**
+ * @param {*} video : Selected Video to place watermark on it
+ * @param {*} watermark : Object like { dir: string; icon: string } to find watermark picture
+ * @param {*} destination : Destination folder to save watermarked video
+ * @param {*} overlay : Position of watermark like (W-w)-20:(H-h)-40
+ * @return {*}
+ */
+export const watermark = (
+  video: string,
+  watermark: { dir: string; icon: string },
+  destination: string,
+  overlay: string = "(W-w)-20:(H-h)-40"
+) => {
+  const iconPath = join(watermark.dir, watermark.icon);
+  return new ffmpeg({ source: video })
+    .addOption(
+      "-vf",
+      `movie=${iconPath} [watermark]; [in] [watermark] overlay=${overlay} [out]`
+    )
+    .on("start", function (commandLine) {
+      console.log(commandLine);
+    })
+    .on("progress", function (progress) {
+      console.log(progress.timemark);
+    })
+    .on("error", function (err, stdout, stderr) {
+      console.log("ERROR: " + err.message);
+      console.log("STDERR:" + stderr);
+    })
+    .on("end", function () {
+      console.log("DONE");
+    })
+    .saveToFile(destination);
 };
